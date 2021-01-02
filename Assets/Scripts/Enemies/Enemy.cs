ec0cac7e65709b137a6bea66f6f7d6fea7a95e03
Anthonyhelka <helkaanthony1997@gmail.com>
2021-01-02 13:33:42 -0500

    Added enemy health bars

--- Assets/Scripts/Enemies/Enemy.cs
+++ Assets/Scripts/Enemies/Enemy.cs
@@ -4,31 +4,35 @@ using UnityEngine;
 using TMPro;
 
 public class Enemy : MonoBehaviour {
+  [SerializeField] private HealthBar _healthBar;
   private Animator _animator;
   private BoxCollider2D _bc;
   [SerializeField] private GameObject _damagePopup;
-  [SerializeField] private int maxHealth = 1;
-  [SerializeField] private int currentHealth;
+  [SerializeField] private int _maxHealth = 100;
+  [SerializeField] private int _currentHealth;
 
   void Awake()
   {
     _animator = GetComponent<Animator>();
     _bc = GetComponent<BoxCollider2D>();
-    currentHealth = maxHealth;
+    _currentHealth = _maxHealth;
+    _healthBar.SetHealth(_currentHealth, _maxHealth);
   }
 
   public void TakeDamage(int damage) {
-    currentHealth -= damage;
+    _currentHealth -= damage;
+    _healthBar.SetHealth(_currentHealth, _maxHealth);
     GameObject damagePopupInstance = Instantiate(_damagePopup, transform.position, Quaternion.identity);
     TextMeshPro damagePopupText = damagePopupInstance.transform.GetChild(0).GetComponent<TextMeshPro>();
     damagePopupText.SetText(damage.ToString());
-    if (currentHealth <= 0) {
+    if (_currentHealth <= 0) {
       damagePopupText.color = Color.red;
       Die();
     }
   }
 
   void Die() {
+    _healthBar.Destroy();
     Patrol patrol = GetComponent<Patrol>();
     if (patrol != null) patrol.enabled = false;
     _bc.enabled = false;
